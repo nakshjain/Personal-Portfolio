0bab63eb469b22b7d1f72d42bd826626150590eb
Nakshatra Jain <nakshatrajain246@gmail.com>
2023-08-11 05:02:20 +0530

    Added Loading in Resume Page

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgxUiLoaderService} from "ngx-ui-loader";
 
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
   styleUrls: ['./resume.component.css']
 })
-export class ResumeComponent {
+export class ResumeComponent implements OnInit{
+  constructor(private _loader: NgxUiLoaderService) {
+  }
+
+  ngOnInit(): void {
+    this._loader.start()
+    setTimeout(()=>{
+      this._loader.stop()
+    }, 1000)
+  }
   public page=1
   public pageLabel!: string
 }
