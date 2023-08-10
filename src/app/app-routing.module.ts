6b206057669500761fc5a01fc0f7cdde0ada5f6f
Nakshatra Jain <nakshatrajain246@gmail.com>
2023-08-10 22:50:33 +0530

    Added '' route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { ProjectsComponent } from './projects/projects.component';
+import {AppComponent} from "./app.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomePageComponent },
   { path: 'about-me', component: AboutMeComponent },
   { path: 'projects', component: ProjectsComponent },
+  { path: '', component: AppComponent}
 ];
 
 @NgModule({
